ac877356305b46c2ba09a16254a58cff8e9eaada
layanqaimari <layan.qaimari@gmail.com>
2026-01-22 09:36:09 -0500

    Homework 2
    Leaflet map with custom popup and custom icon

---
+++ hwmk2/dorotheadix.js
@@ -0,0 +1,7 @@
+var dixpark= {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "descrip": "unc well" }, "geometry": { "type": "Point", "coordinates": [-78.66102818424875, 35.769031399347696 ] } }
+]
+}
